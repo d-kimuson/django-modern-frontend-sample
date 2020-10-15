15b9866b9f33449ae1409c92a96d72c9ed939fb9
Kaito <mail.kaito03@gmail.com>
2020-10-15 14:10:17 +0900

    [update] load base.js from base.html

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webpack_loader',
     'sample.apps.SampleConfig',
 ]
 
@@ -119,3 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    BASE_DIR.parent / 'frontend' / 'dist',
+)
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': '',
+        'STATS_FILE': BASE_DIR.parent / 'frontend' / 'webpack-stats.json',
+    }
+}
